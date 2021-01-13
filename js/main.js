7459db427e89550fd560e9aea8f752f04e512e75
Farrukh Gulomov <@frankywht@gmail.com>
2021-01-13 20:10:23 +0500

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -83,4 +83,19 @@ var x = false;
    
    
 });
-    
+    //with vanilla JavaScript only
+
+// const ready = function (cb) {
+//     // Check if the `document` is loaded completely
+//     document.readyState === "loading"
+//       ? document.addEventListener("DOMContentLoaded", function (e) {
+//           cb();
+//         })
+//       : cb();
+//   };
+  
+//   // Usage
+//   ready(function() {
+//       // Do something when the document is ready
+//       ...
+//   });
